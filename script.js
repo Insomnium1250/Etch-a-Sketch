111a9f1197cffa21425236937c221d64c094c468
Insomnium1250 <quang_evilrocker@yahoo.com.vn>
2024-10-15 19:37:06 +0700

    Finish touches for the project, fully functional

--- script.js
+++ script.js
@@ -5,22 +5,33 @@ function createGrid(numberOfBoxes) {
   for (let i = 0; i < numberOfBoxes ** 2; i++) {
     let div = document.createElement("div");
     div.id = `box${i}`;
-    div.style.backgroundColor = "blue";
-    div.style.width = `calc(90% / ${numberOfBoxes} - 2px)`; // Subtract 2px for the border
-    div.style.height = `calc(90% / ${numberOfBoxes} - 2px)`; // Subtract 2px for the border
+    div.style.backgroundColor = "black";
+    div.style.opacity = "0.1";
+    div.style.width = `calc(960px / ${numberOfBoxes})`;
+    div.style.aspectRatio ="1/1";
     div.style.boxSizing = "border-box";
-    div.style.border = "1px solid white";
+    div.style.border = "solid white 1px";
     container.appendChild(div);
   }
+
+  const squares = document.querySelectorAll("#container *");
+  squares.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+      let currentOpacity = parseFloat(square.style.opacity);
+      if (currentOpacity < 1) {
+        square.style.opacity = (currentOpacity + 0.1).toString();
+      }
+    });
+  });
 }
 
 // Add event listener to the button
 const createGridBtn = document.querySelector("#createGridBtn");
 createGridBtn.addEventListener("click", () => {
   const numberOfBoxes = parseInt(document.querySelector("#gridInput").value);
-  if (numberOfBoxes > 0) {
+  if (numberOfBoxes > 0 && numberOfBoxes <= 100) {
     createGrid(numberOfBoxes); // Create grid with the entered number of boxes
   } else {
-    alert("Please enter a valid number.");
+    alert("Please enter a number from 1 to 100.");
   }
 });
